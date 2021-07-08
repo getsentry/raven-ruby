dd1c32c23d62a2d4dc03b4f2da8fb38e58747377
ojab <ojab@ojab.ru>
2021-07-08 18:58:12 +0000

    Silence method redefinition warnings

--- sentry-ruby/lib/sentry/transaction_event.rb
+++ sentry-ruby/lib/sentry/transaction_event.rb
@@ -5,14 +5,17 @@ module Sentry
     TYPE = "transaction"
 
     ATTRIBUTES = %i(
-      event_id level timestamp start_timestamp
+      event_id level timestamp
       release environment server_name modules
       user tags contexts extra
-      transaction platform sdk type
+      transaction platform sdk
     )
+    HASH_ATTRIBUTES = [*ATTRIBUTES, :type, :start_timestamp, :spans].freeze
 
     attr_accessor(*ATTRIBUTES)
     attr_accessor :spans
+    attr_reader :start_timestamp
+    attr_writer :type
 
     def start_timestamp=(time)
       @start_timestamp = time.is_a?(Time) ? time.to_f : time
